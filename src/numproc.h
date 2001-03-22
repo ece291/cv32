fd3d2c06601d5f8a6ece564b74ca5faccd4f7422
Peter Johnson <peter@tortall.net>
2001-03-22 03:33:46 +0000

    Match #ifdef wrapper with name of class.

--- src/numproc.h
+++ src/numproc.h
@@ -1,9 +1,9 @@
 /* CodeView/32 - TNumericProcessorWindow Header File */
 /* Copyright (c) 2001 by Peter Johnson, pete@bilogic.org */
-/* $Id: numproc.h,v 1.2 2001/03/22 03:29:57 pete Exp $ */
+/* $Id: numproc.h,v 1.3 2001/03/22 03:33:46 pete Exp $ */
 
-#if !defined( __NUMERICPROCESSORVIEWER_H )
-#define __NUMERICPROCESSORVIEWER_H
+#if !defined( __NUMERICPROCESSORWINDOW_H )
+#define __NUMERICPROCESSORWINDOW_H
 
 #define Uses_TWindow
 #include <tv.h>
