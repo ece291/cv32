1748da7a5634f05561400ccb9490afda1db602ff
Peter Johnson <peter@tortall.net>
2001-04-22 21:30:20 +0000

    Repo-copied to /include/

--- src/debug.h
+++
@@ -1,5 +0,0 @@
-/* Copyright (C) 1993 DJ Delorie, see COPYING.DJ for details */
-/* $Id: debug.h,v 1.2 2001/01/31 03:24:06 pete Exp $ */
-extern int can_longjmp;
-extern jmp_buf debugger_jmpbuf;
-void debugger(void);
